62ae51cb7220bf9171b93d9f7c0d9b0faf17ae0a
Lawrence Malloy <lcmalloy@gmail.com>
2022-11-27 00:56:10 -0800

    added tiles for movie availability added css styling for tile

--- amc_clone/src/Component/ShowingAvailability.jsx
+++ amc_clone/src/Component/ShowingAvailability.jsx
@@ -1,11 +1,76 @@
 import '../Styles/App.css';
+import ShowingTile from './ShowingTile';
+import {useState} from 'react';
+
+const testData = [
+  {
+    title: 'Black Panther: Wakanda Forever',
+    showtime: '12:30 - 2:30',
+    location: 'Walnut Creek',
+    seating: 'http://test.com/'
+  },
+  {
+    title: 'Black Panther: Wakanda Forever',
+    showtime: '1:30 - 2:30',
+    location: 'Union City',
+    seating: 'http://test2.com/'
+  },
+  {
+    title: 'Black Panther: Wakanda Forever',
+    showtime: '3:30 - 2:30',
+    location: 'Alameda',
+    seating: 'http://test3.com/'
+  },
+  {
+    title: 'Black Panther: Wakanda Forever',
+    showtime: '12:30 - 2:30',
+    location: 'Walnut Creek',
+    seating: 'http://test.com/'
+  },
+  {
+    title: 'Black Panther: Wakanda Forever',
+    showtime: '1:30 - 2:30',
+    location: 'Union City',
+    seating: 'http://test2.com/'
+  },
+  {
+    title: 'Black Panther: Wakanda Forever',
+    showtime: '3:30 - 2:30',
+    location: 'Alameda',
+    seating: 'http://test3.com/'
+  },
+  {
+    title: 'Black Panther: Wakanda Forever',
+    showtime: '12:30 - 2:30',
+    location: 'Walnut Creek',
+    seating: 'http://test.com/'
+  },
+  {
+    title: 'Black Panther: Wakanda Forever',
+    showtime: '1:30 - 2:30',
+    location: 'Union City',
+    seating: 'http://test2.com/'
+  },
+  {
+    title: 'Black Panther: Wakanda Forever',
+    showtime: '3:30 - 2:30',
+    location: 'Alameda',
+    seating: 'http://test3.com/'
+  }
+]
 
 const ShowingAvailability = () => {
+  const [isShowingDetail, setShowingDetail] = useState([]);
+  const showings = testData.map((showing) =>
+    <ShowingTile info={showing} />
+  );
   return (
     <div className="Available-Showing-Container" id="Availability-Style">
       <div className="Showing-Header">
         Available Showings
-        <div className="Showings-Container"></div>
+      </div>
+      <div className="Showings-Container">
+        {showings}
       </div>
     </div>
   )
---
+++ amc_clone/src/Component/ShowingTile.jsx
@@ -0,0 +1,27 @@
+import '../Styles/ShowingTile.css'
+
+const ShowingTile = (prop) => {
+  const testData = () => {
+    console.log(prop);
+  }
+  return (
+    <div className="ShowingTile_Container">
+      <div className="Showing-Title">
+        {prop.info.title}
+      </div>
+      <div className="Showing-Showtime">
+        {prop.info.showtime}
+      </div>
+      <div className="Showing-Location">
+        {prop.info.location}
+      </div>
+      <div className="Showing-Seating">
+        <a  href={prop.info.seating}>
+          Movie Seating
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default ShowingTile;
